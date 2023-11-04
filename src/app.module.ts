601d9009d2f87bfbafca91118432046b82270235
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-11-03 23:58:25 -0400

    Configuration initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'user',
+      password: 'root',
+      database: 'db_ucab_linkedin',
+      autoLoadEntities : true,
+      synchronize: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env.development' || '.env.production' || '.env.development.local',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
