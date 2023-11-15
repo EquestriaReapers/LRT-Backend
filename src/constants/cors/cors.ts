54ef617d60c8c893d87d0fddad0caec3aef594d9
Paniagua <114714690+luizzzito@users.noreply.github.com>
2023-11-15 11:05:11 -0400

    Cors fixes

--- src/constants/cors/cors.ts
+++ src/constants/cors/cors.ts
@@ -1,7 +1,8 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 export const CORS: CorsOptions = {
-    origin: true,
+    origin: "*",
+    allowedHeaders: '*',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
 }
