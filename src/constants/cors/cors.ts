8e94917aea62ef5711024fe3c32d82ae001adcf2
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-11-15 20:32:32 -0400

    Fixes

--- src/constants/cors/cors.ts
+++ src/constants/cors/cors.ts
@@ -1,12 +1,9 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 export const CORS: CorsOptions = {
-    origin: ['http://localhost:3000',
-            'http://localhost:4200',
-            'http://localhost:8080',
-            'http://localhost:5173',
-        ],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    preflightContinue: false,
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
 }
