a82237fff86d2dd4678e2ae436ba564621a4a129
Luis Rivas <luisrivas.w@gmail.com>
2023-12-22 22:42:21 -0400

    Errores en español :es:

--- src/core/profiles/dto/add-language-profile.dto.ts
+++ src/core/profiles/dto/add-language-profile.dto.ts
@@ -1,14 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsInt, IsString } from 'class-validator';
-import { Language } from 'src/core/language/entities/language.entity';
+import { IsEnum, IsInt } from 'class-validator';
 import { LanguageLevel } from '../entities/language-profile.entity';
 
 export class AddLanguageProfileDto {
   @ApiProperty()
-  @IsInt()
+  @IsInt({ message: 'El ID del idioma debe ser un número entero' })
   languageId: number;
 
   @ApiProperty({ enum: LanguageLevel })
-  @IsEnum(LanguageLevel)
+  @IsEnum(LanguageLevel, { message: 'Nivel de idioma inválido' })
   level: LanguageLevel;
 }
