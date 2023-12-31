e6ebc0dd0f40c338a388428917c983c46ed48252
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-12-30 21:10:33 -0400

    Add Swagger annotations and error handling to LocationController

--- src/core/location/location.controller.ts
+++ src/core/location/location.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { LocationService } from './service/location.service';
-import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiInternalServerError } from 'src/common/decorator/internal-server-error-decorator';
+import { ApiException } from '@nanogiants/nestjs-swagger-api-exception-decorator';
+import { LocationResponse } from './dto/response.dto';
 
 @ApiTags('location')
 @Controller('location')
@@ -8,12 +17,25 @@ export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
   @Get()
+  @ApiOkResponse({
+    description: 'Returns an array of ALL Location',
+    type: [LocationResponse],
+  })
+  @ApiInternalServerError()
   @ApiQuery({ name: 'name', required: false })
   findAll(@Query('name') name: string) {
     return this.locationService.findAll(name);
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    description: 'Returns one Location',
+    type: LocationResponse,
+  })
+  @ApiInternalServerError()
+  @ApiException(() => NotFoundException, {
+    description: 'Location not found',
+  })
   findOne(@Param('id') id: number) {
     return this.locationService.findOne(+id);
   }
