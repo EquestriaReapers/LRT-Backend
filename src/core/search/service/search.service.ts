57d7b0924a2ea1ef49be9b25adf26d24db8dbcd3
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-01-07 03:23:44 -0400

    funciona por fis

--- src/core/search/service/search.service.ts
+++ src/core/search/service/search.service.ts
@@ -118,7 +118,7 @@ export class SearchService {
 
       language = await this.validateQueryArray(language);
 
-      if (career) {
+      if (career && career.length > 0) {
         filter.push({
           bool: {
             should: career.map((career) => ({
@@ -130,7 +130,7 @@ export class SearchService {
         });
       }
 
-      if (skills && Array.isArray(skills)) {
+      if (skills && Array.isArray(skills) && skills.length > 0) {
         if (isExclusiveSkills) {
           skills.forEach((skill) => {
             filter.push({
@@ -164,7 +164,11 @@ export class SearchService {
         }
       }
 
-      if (countryResidence && Array.isArray(countryResidence)) {
+      if (
+        countryResidence &&
+        Array.isArray(countryResidence) &&
+        countryResidence.length > 0
+      ) {
         filter.push({
           bool: {
             should: countryResidence.map((countryResidence) => ({
@@ -176,7 +180,7 @@ export class SearchService {
         });
       }
 
-      if (language && Array.isArray(language)) {
+      if (language && Array.isArray(language) && language.length > 0) {
         if (isExclusiveLanguages) {
           language.forEach((language) => {
             filter.push({
