8e471eb57b24ecc1dd2391836873106162abc3c7
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-01-06 23:13:37 -0400

    Refactor search service to use match instead of term for filtering by mainTitle

--- src/core/search/service/search.service.ts
+++ src/core/search/service/search.service.ts
@@ -123,7 +123,7 @@ export class SearchService {
       if (career) {
         career.forEach((mainTitle) => {
           filter.push({
-            term: {
+            match: {
               mainTitle,
             },
           });
